dc655b88ec4d7b85a97cf2e6630eb78c17937e46
Elian Nasatto <elian.nasa123@gmail.com>
2022-04-06 13:47:27 -0300

    Innitial Commit

---
+++ EstatisticasNomesIBGE/Controllers/NomeController.cs
@@ -0,0 +1,53 @@
+﻿using EstatisticasNomesIBGE.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace EstatisticasNomesIBGE.Controllers
+{
+    public class NomeController : Controller
+    {
+
+        public string BuscaDadosIBGE(string nome)
+        {
+            if (string.IsNullOrEmpty(nome))
+                View("Index");
+
+            WebClient client = new WebClient();
+            return client.DownloadString("https://servicodados.ibge.gov.br/api/v2/censos/nomes/" + nome);
+          
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Pesquisar(string nome)
+        {
+           
+            return View(BuscaDadosIBGE(nome));
+
+        }
+
+        public IActionResult PesquisarGrafico(string nome)
+        {
+            string json = BuscaDadosIBGE(nome);
+            if (string.IsNullOrEmpty(json) || json.Equals("[]"))
+            {
+                ViewBag.Mensagem = "Não foi encontrando dados na base do IBGE";
+                return View("Index");
+            }
+
+         
+            return View(JsonConvert.DeserializeObject<List<Dados>>(json).FirstOrDefault());
+
+        }
+
+
+    }
+}
---
+++ EstatisticasNomesIBGE/Models/EstatisticasIBGE.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstatisticasNomesIBGE.Models
+{
+    public class Dados
+    {
+        public string Nome { get; set; }
+        public object Sexo { get; set; }
+        public string Localidade { get; set; }
+        public Res[] Res { get; set; }
+    }
+
+    public class Res
+    {
+        private string _periodo { get; set; }
+
+        public string Periodo
+        {
+            get { return this._periodo; }
+            set { this._periodo = value.Replace("[", ""); }
+        }
+        public int Frequencia { get; set; }
+    }
+
+    //public class EstatisticasIBGE
+    //{
+    //    public string Nome { get; set; }
+    //    public object Sexo { get; set; }
+    //    public string Localidade { get; set; }
+    //    public List<Estatistica> Estatisticas { get; set; }
+    //}
+
+    //public class Estatistica
+    //{
+    //    public string Periodo { get; set; }
+    //    public int Frequencia { get; set; }
+    //}
+}
